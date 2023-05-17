ec8179010cd80db9cf6f473955dc35cd45240152
anaximeno <anaximenobrito@gmail.com>
2023-05-16 23:51:51 -0100

    chore: update main.py

--- phase_1/task_1_solution/scripts/main.py
+++ phase_1/task_1_solution/scripts/main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import rospy
 
+# TODO: add more code here
 
 if __name__ == '__main__':
-    print('TODO')
-
+    rospy.init_node('task_1_solver')
+    rospy.loginfo('Running Task 1 Solver')
+    rospy.loginfo('Hit \'Ctrl + c\' to quit, or run \'rosnode kill /task_1_solver\'')
+    rospy.spin()
 
