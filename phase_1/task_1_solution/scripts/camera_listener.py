17b63ab44c5e39254864fd746350fa01b1be2127
anaximeno <anaximenobrito@gmail.com>
2023-05-19 22:02:58 -0100

    feat: add ground and plant segmentation to camera listener

--- phase_1/task_1_solution/scripts/camera_listener.py
+++ phase_1/task_1_solution/scripts/camera_listener.py
@@ -2,8 +2,50 @@
 import rospy
 import cv2
 
+import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+
+
+DEFAULT_SEG_CRT = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+MASK = cv2.imread('../images/robot_camera_mask.png', 0)
+
+
+def mask_image(image, mask):
+    return cv2.bitwise_and(image, image, mask=mask)
+
+
+def segment_image(image, attempts = 10, k = 10, criteria = DEFAULT_SEG_CRT):
+    td_img = np.float32(image.reshape((-1, 3)))
+    _, label, center = cv2.kmeans(td_img, k, None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
+
+    center = np.uint8(center)
+    res = center[label.flatten()]
+
+    return res.reshape((image.shape))
+
+
+def detect_segmented_color_profile(image, lower_bound, upper_bound):
+    image_masked = mask_image(cv2.medianBlur(image, 3), MASK)
+    segmented_image = segment_image(image_masked)
+    color_mask = cv2.inRange(segmented_image, lower_bound, upper_bound)
+    return cv2.bitwise_and(segmented_image, segment_image, color_mask)
+
+
+def detect_plants(image):
+    return detect_segmented_color_profile(
+        image=image,
+        lower_bound=np.array([90, 120, 40]),
+        upper_bound=np.array([100, 220, 70])
+    )
+
+
+def detect_ground(image):
+    return detect_segmented_color_profile(
+        image=image,
+        lower_bound=np.array([105, 90, 55])
+        upper_bound=np.array([160, 140, 120])
+    )
 
 
 def callback(data: Image):
@@ -18,7 +60,7 @@ def callback(data: Image):
     br = CvBridge()
     rospy.loginfo('Receiving video frame')
     frame = br.imgmsg_to_cv2(data)
-    cv2.imshow('camera', frame)
+    cv2.imshow('camera', detect_ground(frame))
     cv2.waitKey(1)
 
 
