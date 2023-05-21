86bc8aed6e6d6bcfd5a501bbb6fc7ff5c4572667
anaximeno <anaximenobrito@gmail.com>
2023-05-21 20:43:58 -0100

    chore: made some small updates

--- phase_1/task_1_solution/scripts/main.py
+++ phase_1/task_1_solution/scripts/main.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import rospy
-
 from robot_agent import UcvRobotAgent
 
 
--- phase_1/task_1_solution/scripts/robot_agent.py
+++ phase_1/task_1_solution/scripts/robot_agent.py
@@ -7,8 +7,10 @@ from robot_control import UcvRobotControl
 class UcvRobotAgent:
     def __init__(self, node_id = 'ucv_task_1_solver_robot_agent'):
         self._node_id = node_id
+
         rospy.loginfo(f'Starting Task 1 Solver Agent {self._node_id!r}')
         rospy.init_node(node_id, anonymous = False)
+
         self.sensor = UcvRobotSensor(debug = True)
         self.control = UcvRobotControl(self.sensor)
 
