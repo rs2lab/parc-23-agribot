4a06466054e490d97f57807406a36cba31afbf33
anaximeno <anaximenobrito@gmail.com>
2023-05-21 20:14:28 -0100

    fix: use node_id as an attr

--- phase_1/task_1_solution/scripts/robot_agent.py
+++ phase_1/task_1_solution/scripts/robot_agent.py
@@ -8,11 +8,12 @@ class UcvRobotAgent:
     def __init__(self, node_id = 'ucv_task_1_solver_robot_agent'):
         rospy.loginfo(f'Starting Task 1 Solver Agent {node_id!r}')
         rospy.init_node(node_id, anonymous = False)
+        self._node_id = node_id
         self.sensor = UcvRobotSensor(debug = True)
         self.control = UcvRobotControl(self.sensor)
 
     def run(self):
         rospy.spin()
-        rospy.loginfo(f'Finishing Task 1 Solver Agent {node_id!r}')
+        rospy.loginfo(f'Finishing Task 1 Solver Agent {self._node_id!r}')
 
 
