8c1c08bb33e4e3e31b4eb7f4f4b8ee095f40e4d4
anaximeno <anaximenobrito@gmail.com>
2023-05-21 19:42:39 -0100

    fix: made some fixes

--- phase_1/task_1_solution/scripts/main.py
+++ phase_1/task_1_solution/scripts/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import rospy
+
 from robot_agent import UcvRobotAgent
 
 
--- phase_1/task_1_solution/scripts/robot_agent.py
+++ phase_1/task_1_solution/scripts/robot_agent.py
@@ -5,10 +5,10 @@ from robot_control import UcvRobotControl
 
 
 class UcvRobotAgent:
-    def __init__(self, node_id = '/ucv/task_1_solver/robot_agent'):
+    def __init__(self, node_id = 'ucv_task_1_solver_robot_agent'):
         rospy.init_node(node_id, anonymous = False)
         self.sensor = UcvRobotSensor(debug = True)
-        self.control = UcvRobotControl(self._sensor)
+        self.control = UcvRobotControl(self.sensor)
 
     def run(self):
         rospy.spin()
