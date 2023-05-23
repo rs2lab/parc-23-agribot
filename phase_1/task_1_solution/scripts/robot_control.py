0ae47d4b76e6a74b35fe2d4a5960c9fa8af60609
anaximeno <anaximenobrito@gmail.com>
2023-05-23 21:04:33 -0100

    feat: add robot planner
    refactor: update the responsability delegation

--- phase_1/task_1_solution/scripts/robot_control.py
+++ phase_1/task_1_solution/scripts/robot_control.py
@@ -5,58 +5,13 @@ from geometry_msgs.msg import Twist
 from robot_perception import UcvSensorType
 
 
-# NOTE: be aware that we might have to face a 'race condition' (a concurrency problem)
-#   if the state of the robot control object is accessed by the callbacks when the
-#   sensors' states update, because two or more callbacks can be called almost simultaneously.
-
-
 class UcvRobotControl:
-    def __init__(self, robot_perception, default_publishing_rate = 10):
-        self.default_publishing_rate = default_publishing_rate
-        self._perception = robot_perception
+    def __init__(self):
+        self._cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
 
-        self._perception.register_state_update_callback(
-            sensor_type = UcvSensorType.CAM_LEFT,
-            callback = self.on_left_camera_state_update,
-        )
-        self._perception.register_state_update_callback(
-            sensor_type = UcvSensorType.CAM_RIGHT,
-            callback = self.on_right_camera_state_update,
-        )
-        self._perception.register_state_update_callback(
-            sensor_type = UcvSensorType.CAM_FRONT,
-            callback = self.on_front_camera_state_update,
-        )
-        self._perception.register_state_update_callback(
-            sensor_type = UcvSensorType.GPS,
-            callback = self.on_gps_state_update,
-        )
-        self._perception.register_state_update_callback(
-            sensor_type = UcvSensorType.LASER_SCAN,
-            callback = self.on_laser_scan_state_update,
-        )
-        self._perception.register_state_update_callback(
-            sensor_type = UcvSensorType.CMD_VEL,
-            callback = self.on_cmd_vel_state_update
-        )
+    @property
+    def cmd_vel_pub(self):
+        return self._cmd_vel_pub
 
-        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
-
-    def on_front_camera_state_update(self, data):
-        pass # TODO: react to state update?
-
-    def on_left_camera_state_update(self, data):
-        pass # TODO: react to state update?
-
-    def on_right_camera_state_update(self, data):
-        pass # TODO: react to state update?
-
-    def on_gps_state_update(self, data):
-        pass # TODO: react to state update?
-
-    def on_laser_scan_state_update(Self, data):
-        pass # TODO: react to state update?
-
-    def on_cmd_vel_state_update(self, data):
-        # NOTE: Be careful here since this is updated mainly by us
-        pass # TODO: react to state update?
+    def publish_cmd_vel(twist_move_cmd):
+        self._cmd_vel_pub.publish(twist_move_cmd)
---
+++ phase_1/task_1_solution/scripts/robot_planner.py
@@ -0,0 +1,75 @@
+import rospy
+import cv2
+
+from robot_perception import UcvSensorType
+
+
+class UcvRobotPlanner:
+    def __init__(self, control, perception):
+        self._control = control
+        self._perception = perception
+
+        # NOTE: be aware that we might have to face a 'race condition' (a concurrency problem)
+        #   if the state of the robot planner object is accessed by the callbacks when the
+        #   sensors' states update, because two or more callbacks can be called almost simultaneously.
+
+        self._perception.register_state_update_callback(
+            sensor_type=UcvSensorType.CAM_LEFT,
+            callback=self.on_left_camera_state_update,
+        )
+        self._perception.register_state_update_callback(
+            sensor_type=UcvSensorType.CAM_RIGHT,
+            callback=self.on_right_camera_state_update,
+        )
+        self._perception.register_state_update_callback(
+            sensor_type=UcvSensorType.CAM_FRONT,
+            callback=self.on_front_camera_state_update,
+        )
+        self._perception.register_state_update_callback(
+            sensor_type=UcvSensorType.GPS,
+            callback=self.on_gps_state_update,
+        )
+        self._perception.register_state_update_callback(
+            sensor_type=UcvSensorType.LASER_SCAN,
+            callback=self.on_laser_scan_state_update,
+        )
+        self._perception.register_state_update_callback(
+            sensor_type=UcvSensorType.CMD_VEL,
+            callback=self.on_cmd_vel_state_update
+        )
+
+    def plan(self):
+        """Analyse the information from the perception mechanisms
+        and determine the best course of action to be taken by the robot."""
+        pass # TODO
+
+    def execute(self, hz = 10):
+        """Execute the plan using the control mechanisms to achieve the goal."""
+        rospy.loginfo('-- Initializing UcvRobotPlanner execution...')
+
+        rate = rospy.Rate(hz)
+
+        while not rospy.is_shutdown():
+            # TODO: plan and execute the action
+            rate.sleep()
+
+        rospy.loginfo('-- Terminating UcvRobotPlanner execution.')
+
+    def on_front_camera_state_update(self, data):
+        pass # TODO: react to state update?
+
+    def on_left_camera_state_update(self, data):
+        pass # TODO: react to state update?
+
+    def on_right_camera_state_update(self, data):
+        pass # TODO: react to state update?
+
+    def on_gps_state_update(self, data):
+        pass # TODO: react to state update?
+
+    def on_laser_scan_state_update(Self, data):
+        pass # TODO: react to state update?
+
+    def on_cmd_vel_state_update(self, data):
+        # NOTE: Be careful here since this is updated mainly by us
+        pass # TODO: react to state update?
