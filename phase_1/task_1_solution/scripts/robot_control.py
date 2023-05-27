2e93a314e16c4e4c9557fae29e8728a34e2e2263
anaximeno <anaximenobrito@gmail.com>
2023-05-27 12:29:30 -0100

    refactor: renamed cmd pub method

--- phase_1/task_1_solution/scripts/robot_control.py
+++ phase_1/task_1_solution/scripts/robot_control.py
@@ -17,7 +17,7 @@ class UcvRobotControl:
     def set_publish_rate(self, hz):
         self.rate = rospy.Rate(hz)
 
-    def publish_to_cmd_vel(self, twist, secs=0):
+    def publish_cmd_vel(self, twist, secs=0):
         now = time.time()
         while secs == 0 or (sep := time.time() - now) < secs:
             if self._debug is True:
@@ -40,13 +40,13 @@ class UcvRobotControl:
         cmd.angular.x = 0.0
         cmd.angular.y = 0.0
         cmd.angular.z = 0.0
-        self.publish_to_cmd_vel(cmd, 0)
+        self.publish_cmd_vel(cmd, 0)
 
     def move_regular(self, x=0, theta=0, secs=1):
         twist = Twist()
         twist.linear.x = x
         twist.angular.z = theta
-        self.publish_to_cmd_vel(twist, secs)
+        self.publish_cmd_vel(twist, secs)
 
     def execute_plan(self, plan):
-        self.publish_to_cmd_vel(plan.to_twist(), plan.secs)
+        self.publish_cmd_vel(plan.to_twist(), plan.secs)
