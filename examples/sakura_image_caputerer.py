a8c3338a7bc4fe28f25e461fcdbd4e69ac8bebea
anaximeno <anaximenobrito@gmail.com>
2023-07-27 14:07:58 -0100

    chore: add code to capture images from topics

---
+++ examples/sakura_image_caputerer.py
@@ -0,0 +1,25 @@
+import rospy
+import cv2
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+TOPIC='/front/zed_nodelet_front/rgb_raw/image_raw_color'
+
+counter=0
+
+def imgmsg_to_cv2(imgmsg):
+    return CvBridge().imgmsg_to_cv2(imgmsg)
+
+def handler(data):
+    global counter
+    image = imgmsg_to_cv2(data)
+    counter += 1
+    cv2.imwrite(f'{counter}.jpg', image)
+
+rospy.init_node('recorder')
+rospy.Subscriber(TOPIC, Image, handler)
+
+while not rospy.is_shutdown():
+    print('executing, counter = %d' % counter)
+
