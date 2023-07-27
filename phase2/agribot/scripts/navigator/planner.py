12bce737588365e1553c2b25e8cfdda229d3778e
anaximeno <anaximenobrito@gmail.com>
2023-07-27 16:29:30 +0000

    chore: update the planner

--- phase2/agribot/scripts/navigator/planner.py
+++ phase2/agribot/scripts/navigator/planner.py
@@ -1,14 +1,40 @@
 import rospy
 
-from utils.action import SteppedAction
 from perceptor import AgribotPerceptor
+from utils.action import SteppedAction, Action
+
+from utils.helpers import (
+    ForgetfulMemory,
+    BasicQueue
+)
 
 
 class AgribotPlanner:
     def __init__(self, perception: AgribotPerceptor) -> None:
         self._perception = perception
+        self._last_actions_memory = ForgetfulMemory()
+        self._next_actions_queue = BasicQueue()
 
-    def plan_action(self) -> None:
-        pass # TODO
+    @property
+    def _has_enqueued_actions(self):
+        return not self._next_actions_queue.empty()
+
+    def enqueue_action(self, action: Action) -> None:
+        """Add a new action to the action queue."""
+        self._next_actions_queue.enqueue(action)
+
+    def _resolve_enqueued_actions(self) -> Action | None:
+        """Returns the next action in the queue, removing it from the structure."""
+        if self._has_enqueued_actions:
+            return self._next_actions_queue.dequeue()
+        return None
+
+    def plan_action(self) -> Action | None:
+        """Analyse the information from the perception mechanisms and determine the
+        best course of action to be taken by the robot."""
+        if self._has_enqueued_actions:
+            return self._resolve_enqueued_actions()
+        # TODO
+        return None
 
 
