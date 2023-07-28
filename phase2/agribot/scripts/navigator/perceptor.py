dfd68fc75aad83ff21f5627afcf7f5a08dad579a
anaximeno <anaximenobrito@gmail.com>
2023-07-28 13:58:10 +0000

    chore: update perceptor

--- phase2/agribot/scripts/navigator/perceptor.py
+++ phase2/agribot/scripts/navigator/perceptor.py
@@ -3,10 +3,13 @@ import rospy
 
 from types import FunctionType
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image, LaserScan
 
 
 class SensorType(enum.Enum):
     POINT_CLOUD = '/front/zed_nodelet_front/point_cloud/cloud_registered'
+    FRONT_CAM = '/front/zed_nodelet_front/rgb_raw/image_raw_color'
+    # LEFT_CAM = '' # TODO
     ODOM = '/odom'
     LASER = '/scan'
 
@@ -14,16 +17,21 @@ class SensorType(enum.Enum):
 class AgribotPerceptor:
     def __init__(self) -> None:
         self._point_cloud = None
-        self._odom = None
         self._laser_scan = None
+        self._front_cam = None
+        self._odom = None
 
         self._state_update_cb_registry = {
             SensorType.POINT_CLOUD: [],
+            SensorType.FRONT_CAM: []
             SensorType.ODOM: [],
             SensorType.LASER: [],
         }
 
-        rospy.Subscriber(SensorType.ODOM.value, Odometry, )
+        rospy.Subscriber(SensorType.ODOM.value, Odometry, self._odom_state_update_handler)
+        rospy.Subscriber(SensorType.LASER_SCAN.value, LaserScan, self._laser_scan_state_update_handler)
+        rospy.Subscriber(SensorType.FRONT_CAM.value, Image, self._front_cam_state_update_handler)
+        # rospy.Subscriber(SensorType.POINT_CLOUD.value, ) # TODO
 
     def register_state_update_callback(self, sensor_type: SensorType, callback) -> None:
         """Add callbacks that will be called when the state of one the topics we listen to
@@ -33,7 +41,7 @@ class AgribotPerceptor:
             return True
         return False
 
-    def _trigger_callbacks(self, sensor_type, data):
+    def _trigger_callbacks(self, sensor_type, data) -> None:
         """This triggers all registered callbacks for the specific sensor."""
         if sensor_type in self._state_update_callback_registry:
             callbacks = self._state_update_callback_registry[sensor_type]
@@ -41,16 +49,20 @@ class AgribotPerceptor:
                 callback(data)
 
     @property
-    def point_cloud(self):
+    def point_cloud_state(self):
         return self._point_cloud
 
     @property
-    def odom(self):
+    def odom_state(self) -> Odometry:
         return self._odom
 
     @property
-    def laser_scan(self):
+    def laser_scan_state(self) -> LaserScan:
         return self._laser_scan
+
+    @property
+    def front_cam_state(self) -> Image:
+        return self._front_cam
 
     def _laser_scan_state_update_handler(self, data) -> None:
         self._laser_scan = data
@@ -63,3 +75,7 @@ class AgribotPerceptor:
     def _point_cloud_state_update_handler(self, data) -> None:
         self._point_cloud = data
         self._trigger_callbacks(SensorType.POINT_CLOUD, data)
+
+    def _front_cam_state_update_handler(self, data) -> None:
+        self._front_cam = data
+        self._trigger_callbacks(SensorType.FRONT_CAM, data)
