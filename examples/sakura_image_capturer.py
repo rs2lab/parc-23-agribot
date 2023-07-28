f739bdc55e8384ccd059dfc9ea2f3a84de3be226
anaximeno <anaximenobrito@gmail.com>
2023-07-28 19:19:26 -0100

    chore: update sakura img capturer

--- examples/sakura_image_capturer.py
+++ examples/sakura_image_capturer.py
@@ -1,10 +1,21 @@
+#!/usr/bin/python3
+
+import os
 import rospy
 import cv2
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
+
 TOPIC='/front/zed_nodelet_front/rgb_raw/image_raw_color'
+
+
+try:
+    TOPIC=os.getenv('TOPIC', TOPIC)
+except:
+    pass)
+
 
 counter=0
 
@@ -15,9 +26,9 @@ def handler(data):
     global counter
     image = imgmsg_to_cv2(data)
     counter += 1
-    cv2.imwrite(f'{counter}.jpg', image)
+    cv2.imwrite(f'{counter}.png', image)
 
-rospy.init_node('recorder')
+rospy.init_node('sakura_recorder', anonymous=True)
 rospy.Subscriber(TOPIC, Image, handler)
 
 while not rospy.is_shutdown():
