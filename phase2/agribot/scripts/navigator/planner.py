789ac0c60bb38c9ce1cd34d01815e7a7c46ba76f
anaximeno <anaximenobrito@gmail.com>
2023-07-29 08:59:13 +0000

    chore: implement planner base structure

--- phase2/agribot/scripts/navigator/planner.py
+++ phase2/agribot/scripts/navigator/planner.py
@@ -1,5 +1,8 @@
 import rospy
 
+import .utils.vision as v
+import .utils.ruler as r
+
 from .perceiver import AgribotPerceiver
 from .utils import SteppedAction, Action
 from .utils import ForgetfulMemory, BasicQueue
@@ -15,7 +18,7 @@ class AgribotPlanner:
     def _has_enqueued_actions(self) -> bool:
         return not self._next_actions_queue.empty()
 
-    def _resolve_enqueued_actions(self) -> Action:
+    def _resolve_enqueued_action(self) -> Action:
         """Returns the next action in the queue, removing it from the structure."""
         if self._has_enqueued_actions:
             return self._next_actions_queue.dequeue()
@@ -25,10 +28,28 @@ class AgribotPlanner:
         """Add a new action to the action queue."""
         self._next_actions_queue.enqueue(action)
 
+    def _process_env_perception(self) -> dict:
+        return {} # TODO
+
     def plan_action(self) -> Action:
-        """Analyse the information from the perception mechanisms and determine the
+        """Analyse the information from the environment and determine the
         best course of action to be taken by the robot."""
         if self._has_enqueued_actions:
-            return self._resolve_enqueued_actions()
-        # TODO
-        return None
+            return self._resolve_enqueued_action()
+
+        kwargs = self._process_env_perception()
+
+        if (finish := self._finish_navigation(**kwargs)) is not None:
+            return finish
+        elif (turn := self._make_a_turn(**kwargs)) is not None:
+            return turn
+        return self._move_forward(**kwargs)
+
+    def _make_a_turn(self) -> Action:
+        return None # TODO
+
+    def _move_forward(self) -> Action:
+        return None # TODO
+
+    def _finish_navigation(self) -> Action:
+        return None # TODO
